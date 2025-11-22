34c79fa25d55f99763dcb0ce9ad6254358fcdbf6
Abhaya Jung Khadka <jungabhayakhadka333@gmail.com>
2025-11-22 13:18:29 +0545

    SIXTH Classs

---
+++ sixthclass/Exercise.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number of num you wish to make the sum out of:  "))
+count = 1
+sum = 0
+while count<=number:
+    num = int(input(f"Enter {count}st number:  "))
+    
+    count+=1
+    
+print(sum)
